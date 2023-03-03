88ae73ab7e2a919eddc20a35ad1ae41dcee3d2c0
Kongnyuy Livingston <112919571+LivingstonTardzenyuy@users.noreply.github.com>
2023-03-03 20:52:31 +0100

    insertion sort
    insertion sort

---
+++ SortingAlgo/insertionSort/indexx.py
@@ -0,0 +1,12 @@
+def insertionSort(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i-1
+        while j>=0 and key > array[j]:
+            array[j+1] = array[j]
+            j-=1
+        array[j+1] = key
+    print(array)
+list=[9,3,6,99,4,1,2,44,6,2,-4,-6,-2]
+# print('l')
+insertionSort(list)
